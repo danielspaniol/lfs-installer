1457f5d1f8f552da39156f33fa395c906adfd47f
Daniel Spaniol <mail@daniel-spaniol.de>
2019-06-18 11:18:54 +0200

    Include script to build `shadow`
    This is needed so that we can have user-based package management.

---
+++ tmp/shadow.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sed -i 's/groups$(EXEEXT) //' src/Makefile.in
+find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
+find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
+find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
+
+sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@' \
+       -e 's@/var/spool/mail@/var/mail@' etc/login.defs
+
+sed -i 's/1000/999/' etc/useradd
+
+./configure --prefix=/tools --with-group-name-max-length=32
+
+make || exit 1
+
+cp src/su /tools/bin
+
